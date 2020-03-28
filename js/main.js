44cd11d694d2b329470643baf4ee141402cc642c
Oleg Mursalov <olofovich@mail.ru>
2020-03-28 15:05:35 +0300

    пока не понимаю, как отрендерить меню из React компонента!
    код надо дорабатывать

--- js/main.js
+++ js/main.js
@@ -1,34 +1,75 @@
-var mainMenuComponent = React.createClass({
-    propTypes: {
-        items: React.PropTypes.array.isRequired
+MainCustomLogic = {
+    mainMenuComponent: function() {
+        return React.createClass({
+            propTypes: {
+                items: React.PropTypes.array.isRequired
+            },
+            render: function() {
+                var items = this.props.items;
+                if (items && Array.isArray(items)) {
+                    items.map((x) => React.DOM.li(null, ));
+                } else {
+                    return null;
+                }
+                /*<ul class="nav nav-pills">
+                                  <li class="active"><a href="#">Главная</a></li>
+                                  <li class="dropdown ml-3">
+                                      <a href="#" data-toggle="dropdown" class="dropdown-toggle">
+                                          Меню
+                                          <b class="caret"></b>
+                                      </a>
+                                      <ul class="dropdown-menu">
+                                          <li class="text-center"><a href="#">Текст подпункта</a></li>
+                                          <li class="text-center"><a href="#">Текст подпункта</a></li>
+                                          <li class="text-center"><a href="#">Текст подпункта</a></li>
+                                      </ul>
+                                  </li>
+                                  <li class="dropdown ml-3">
+                                      <a href="#" data-toggle="dropdown" class="dropdown-toggle">
+                                          Меню
+                                          <b class="caret"></b>
+                                      </a>
+                                      <ul class="dropdown-menu">
+                                          <li class="text-center"><a href="#">Текст подпункта</a></li>
+                                          <li class="text-center"><a href="#">Текст подпункта</a></li>
+                                          <li class="text-center"><a href="#">Текст подпункта</a></li>
+                                      </ul>
+                                  </li>
+                              </ul>*/
+            }
+        });
     },
-    render: function () {
-        return (
-            <ul class="nav nav-pills">
-                            <li class="active"><a href="#">Главная</a></li>
-                            <li class="dropdown ml-3">
-                                <a href="#" data-toggle="dropdown" class="dropdown-toggle">
-                                    Меню
-                                    <b class="caret"></b>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li class="text-center"><a href="#">Текст подпункта</a></li>
-                                    <li class="text-center"><a href="#">Текст подпункта</a></li>
-                                    <li class="text-center"><a href="#">Текст подпункта</a></li>
-                                </ul>
-                            </li>
-                            <li class="dropdown ml-3">
-                                <a href="#" data-toggle="dropdown" class="dropdown-toggle">
-                                    Меню
-                                    <b class="caret"></b>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li class="text-center"><a href="#">Текст подпункта</a></li>
-                                    <li class="text-center"><a href="#">Текст подпункта</a></li>
-                                    <li class="text-center"><a href="#">Текст подпункта</a></li>
-                                </ul>
-                            </li>
-                        </ul>
+    initialize: function() {
+        var mainMenuComponent = MainCustomLogic.mainMenuComponent;
+        var items = [
+            [
+                "Main shope"
+            ]
+            [
+                "Cars",
+                [
+                    "Mazda",
+                    "Toyota",
+                    "Infinity",
+                    "BMW",
+                    "Mercedec"
+                ]
+            ],
+            [
+                "PC",
+                [
+                    "Asus",
+                    "Asus laptop",
+                    "ACER",
+                    "Mac book pro"
+                ]
+            ]
+        ];
+        ReactDOM.render(
+            React.createElement(mainMenuComponent, items),
+            document.getElementById('main-menu')
         );
     }
-});
+}
+
+MainCustomLogic.initialize();
