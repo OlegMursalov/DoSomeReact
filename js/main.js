4b583290c74e26b3b5670275dcd9664383805bc0
Oleg Mursalov <olofovich@mail.ru>
2020-03-28 18:31:12 +0300

    Работаю с состоянием! Мало опыта

--- js/main.js
+++ js/main.js
@@ -1,44 +1,57 @@
 MainCustomLogic = {
-    mainMenuComponent: function() {
-        return React.createClass({
-            propTypes: {
-                items: React.PropTypes.array.isRequired
-            },
-            render: function() {
-                var items = this.props.items;
-                if (items && Array.isArray(items)) {
-                    items.map((x) => React.DOM.li(null, ));
-                } else {
-                    return null;
-                }
-                /*<ul class="nav nav-pills">
-                                  <li class="active"><a href="#">Главная</a></li>
-                                  <li class="dropdown ml-3">
-                                      <a href="#" data-toggle="dropdown" class="dropdown-toggle">
-                                          Меню
-                                          <b class="caret"></b>
-                                      </a>
-                                      <ul class="dropdown-menu">
-                                          <li class="text-center"><a href="#">Текст подпункта</a></li>
-                                          <li class="text-center"><a href="#">Текст подпункта</a></li>
-                                          <li class="text-center"><a href="#">Текст подпункта</a></li>
-                                      </ul>
-                                  </li>
-                                  <li class="dropdown ml-3">
-                                      <a href="#" data-toggle="dropdown" class="dropdown-toggle">
-                                          Меню
-                                          <b class="caret"></b>
-                                      </a>
-                                      <ul class="dropdown-menu">
-                                          <li class="text-center"><a href="#">Текст подпункта</a></li>
-                                          <li class="text-center"><a href="#">Текст подпункта</a></li>
-                                          <li class="text-center"><a href="#">Текст подпункта</a></li>
-                                      </ul>
-                                  </li>
-                              </ul>*/
+    clockComponent: React.createClass({
+        propTypes: {
+            dTime: React.PropTypes.instanceOf(Date),
+        },
+        getDefaultProps: function() {
+            return {
+                dTime: new Date(),
+            };
+        },
+        render: function() {
+            return React.DOM.div(null, 
+                React.DOM.span(null, this.props.dTime.toLocaleTimeString())
+            );
+        }
+    }),
+    mainMenuComponent: React.createClass({
+        propTypes: {
+            items: React.PropTypes.array.isRequired
+        },
+        render: function() {
+            var items = this.props.items;
+            if (items && Array.isArray(items)) {
+                items.map((x) => React.DOM.li(null, ));
+            } else {
+                return null;
             }
-        });
-    },
+            /*<ul class="nav nav-pills">
+                              <li class="active"><a href="#">Главная</a></li>
+                              <li class="dropdown ml-3">
+                                  <a href="#" data-toggle="dropdown" class="dropdown-toggle">
+                                      Меню
+                                      <b class="caret"></b>
+                                  </a>
+                                  <ul class="dropdown-menu">
+                                      <li class="text-center"><a href="#">Текст подпункта</a></li>
+                                      <li class="text-center"><a href="#">Текст подпункта</a></li>
+                                      <li class="text-center"><a href="#">Текст подпункта</a></li>
+                                  </ul>
+                              </li>
+                              <li class="dropdown ml-3">
+                                  <a href="#" data-toggle="dropdown" class="dropdown-toggle">
+                                      Меню
+                                      <b class="caret"></b>
+                                  </a>
+                                  <ul class="dropdown-menu">
+                                      <li class="text-center"><a href="#">Текст подпункта</a></li>
+                                      <li class="text-center"><a href="#">Текст подпункта</a></li>
+                                      <li class="text-center"><a href="#">Текст подпункта</a></li>
+                                  </ul>
+                              </li>
+                          </ul>*/
+        }
+    }),
     initialize: function() {
         var mainMenuComponent = MainCustomLogic.mainMenuComponent;
         var items = [
